8c6a021fa11f3aee81978f479e256542af7a69f4
Vaishnavi Janardhan <vaish.janardhan@gmail.com>
2020-07-21 15:41:38 +0530

    Use Lombok for auto-generated constructors

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -1,12 +1,8 @@
 package com.google.sps.data;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public final class Comment {
-    
   private final long id;
   private final String message;
-
-  public Comment(long id, String message) {
-    this.id = id;
-    this.message = message;
-  }
 }
